0d969f9dd84b5abccfe32e26272bf684123343b8
Inspur-Hanxu <hanxu04@inspur.com>
2020-02-01 20:45:47 +0800

    上传manifest文件

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Management',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'Alan Hou',
+    'depends': ['base'],
+    'data': [
+        'views/library_menu.xml',
+    ],
+    'application': True,
+}
