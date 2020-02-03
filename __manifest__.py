87a24dfbc716cdaf1944a9b29f151f9ad163cb08
Inspur-Hanxu <60465757+Inspur-Hanxu@users.noreply.github.com>
2020-02-03 17:10:56 +0800

    上传library_app

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Library Management',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'Alan Hou',
+    'depends': ['base'],
+    'data': [
+        'views/library_menu.xml',
+    ],
+    'application': True,
+}
